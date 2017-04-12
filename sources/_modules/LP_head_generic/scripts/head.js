8709f9e93533450405329b87e7c6445e8f14fa87
Paulo Fagundes <paulo.fagundes@gfi.pt>
2017-04-12 17:49:51 +0100

    +add: template de artigo

---
+++ sources/_modules/LP_head_generic/scripts/head.js
@@ -0,0 +1,20 @@
+var justica = justica || {};
+
+justica.CONTROL_head_home = (function () {
+
+    return {
+        init: function (element, data) {
+                this.search();
+                debugger;
+            },
+            search: function () {
+                debugger;
+
+            }
+
+    }
+
+
+
+
+});
